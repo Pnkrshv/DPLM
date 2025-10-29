f419d0edef727c5c4a5846b744c5afb1c48f4c34
Sergei <pankrashovsergei@gmail.com>
2025-10-29 23:23:21 +0300

    DB integration

--- backEnd/main.go
+++ backEnd/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"log"
 	"net/http"
-	"github.com/labstack/echo/v4/middleware"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB
+
+func initDB() {
+	dsn := "host=localhost user=postgres password=Cergey27249 dbname=postgres port=5432 sslmode=disable"
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		log.Fatalf("Ошибка при подключении к базе данных: %v", err)
+	}
+
+	if err := db.AutoMigrate(&loginData{}); err != nil {
+		log.Fatalf("Ошибка в миграции БД: %v", err)
+	}
+}
 
 type loginData struct {
 	Login    string `json:"login"`
 	Password string `json:"password"`
 }
 
-func requestHandler(c echo.Context) error {
+func getAuth(c echo.Context) error {
 	data := new(loginData)
 
 	if err := c.Bind(data); err != nil {
@@ -32,6 +52,6 @@ func requestHandler(c echo.Context) error {
 func main() {
 	e := echo.New()
 	e.Use(middleware.CORS())
-	e.POST("/login", requestHandler)
+	e.POST("/login", getAuth)
 	e.Start(":8080")
 }
