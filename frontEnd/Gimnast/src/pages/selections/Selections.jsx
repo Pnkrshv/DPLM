d37d8e7c284d38d640376be5a3417464ce17f43d
Sergei <pankrashovsergei@gmail.com>
2026-01-19 18:19:13 +0300

    111

--- frontEnd/Gimnast/src/pages/selections/Selections.jsx
+++ frontEnd/Gimnast/src/pages/selections/Selections.jsx
@@ -1,17 +1,121 @@
-import { useState } from 'react'
-// import './Selections.css'
+import { useState } from "react";
+import "./Selections.css";
 
-export default function Selections(){
+export default function Selections() {
+  const [isWindowOpen, setIsWindowOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState("parameters");
 
-    const [isWindowOpen, setIsWindowOpen] = useState(false)
-
-    return(
+  return (
+    <>
+      {isWindowOpen && (
         <>
-            <div className="buttons">
+          <div className="window-bg"></div>
+          <div className="modal-window">
+            <div className="window-navigation">
+              <div className="nav-elements">
+                <div
+                  className="tab-indicator"
+                  style={{
+                    transform:
+                      activeTab === "data"
+                        ? "translateX(0%)"
+                        : "translateX(190%)",
+                    width: activeTab === "data" ? "50%" : "35%",
+                  }}
+                ></div>
+                <div
+                  className={`nav-element ${activeTab === "data" ? "activeTab" : ""}`}
+                  onClick={() => {
+                    setActiveTab("data");
+                  }}
+                >
+                  Данные выборки
+                </div>
+                <div
+                  className={`nav-element ${activeTab === "parameters" ? "activeTab" : ""}`}
+                  onClick={() => {
+                    setActiveTab("parameters");
+                  }}
+                >
+                  Параметры
+                </div>
+              </div>
+              <div
+                className="close-btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsWindowOpen(false);
+                }}
+              >
+                <svg
+                  width="32px"
+                  height="32px"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <g clip-path="url(#clip0_429_11083)">
+                      {" "}
+                      <path
+                        d="M7 7.00006L17 17.0001M7 17.0001L17 7.00006"
+                        stroke="#292929"
+                        stroke-width="2.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      ></path>{" "}
+                    </g>{" "}
+                    <defs>
+                      {" "}
+                      <clipPath id="clip0_429_11083">
+                        {" "}
+                        <rect width="24" height="24" fill="white"></rect>{" "}
+                      </clipPath>{" "}
+                    </defs>{" "}
+                  </g>
+                </svg>{" "}
+              </div>
+            </div>
+            <div className="window-settings"></div>
+
+            <div className="window-content">
+              {activeTab === "parameters" && (
+                <>
+                  <form method="post" className="parameters-form">
+                    <label>Название выборки</label>
+                    <input type="text" />
+                    <label>Тип выборки</label>
+                    <select>
+                      <option disabled selected>Выбрать</option>
+                      <option>Выборка 1</option>
+                      <option>Выборка 2</option>
+                    </select>
+                  </form>
+                </>
+              )}
+
+              {activeTab === "data" && (
+                <>
+                  <p>data</p>
+                </>
+              )}
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="buttons">
         <button
           className="create-btn"
           onClick={() => {
-            setIsWindowOpen(true)
+            setIsWindowOpen(true);
           }}
         >
           <p>Создать выборку</p>
@@ -22,17 +126,6 @@ export default function Selections(){
           </svg>
         </button>
       </div>
-
-          {isWindowOpen && (<>
-            <div className="modal-window">
-                <div className="window-navigation">
-                    <div className="nav-element">Данные выборки</div>
-                    <div className="nav-element">Параметры</div>
-                </div>
-                <div className="window-settings"></div>
-            </div>
-          </>)}
-
-        </>
-    )
+    </>
+  );
 }
