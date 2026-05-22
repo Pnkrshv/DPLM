2b16a0854498278b1d3afb43b09092445cbfc086
Sergei <pankrashovsergei@gmail.com>
2026-05-22 11:47:26 +0300

    upd

--- frontEnd/Gimnast/src/pages/Questionnaires/Questionnaires.jsx
+++ frontEnd/Gimnast/src/pages/Questionnaires/Questionnaires.jsx
@@ -1099,8 +1099,8 @@ export default function Questionnaires() {
                 }}
               >
                 <svg
-                  width="32px"
-                  height="32px"
+                  width="18px"
+                  height="18px"
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
